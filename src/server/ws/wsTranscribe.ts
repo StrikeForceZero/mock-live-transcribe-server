e8a43268ccedc18b4115f3217407b517710f3f24
bstriker <strikeforcezero@gmail.com>
2025-03-21 22:27:08 -0400

    refactor(wss): cleanup wss

--- src/server/ws/wsTranscribe.ts
+++ src/server/ws/wsTranscribe.ts
@@ -11,7 +11,7 @@ import {
 import { timeout } from '@util/timeout';
 import { getTokenFromAuthorization, getUserIdFromToken } from '@server/auth';
 import * as transcribeService from '@server/services/trascribeService';
-import { UserId } from '@server/types';
+import { userId, UserId } from '@server/types';
 
 export enum WsCloseCode {
   Normal = 1000,
@@ -71,6 +71,7 @@ export function bufferFromRawData(rawData: RawData): Buffer {
 }
 
 const USER_ID_SOCKET_MAP: Record<UserId, WebSocket | undefined> = {};
+const USER_ID_READY_SOCKET_MAP: Record<UserId, boolean | undefined> = {};
 
 function registerSocketForUserId(clientSocket: WebSocket, userId: UserId) {
   const existingSocket = USER_ID_SOCKET_MAP[userId];
@@ -84,6 +85,7 @@ function registerSocketForUserId(clientSocket: WebSocket, userId: UserId) {
     );
   }
   USER_ID_SOCKET_MAP[userId] = clientSocket;
+  USER_ID_READY_SOCKET_MAP[userId] = false;
 }
 
 export type CloseReasonObj = { error: unknown; code: InternalErrorCode };
@@ -149,12 +151,129 @@ function sendData(
   });
 }
 
-export function setupWebSocket(server: Server) {
-  const wss = new WebSocketServer<AuthenticatedWebSocket>({
-    server,
-  });
+function isOpen(socket: WebSocket): boolean {
+  return socket.readyState === WebSocket.OPEN;
+}
 
-  async function shutdown() {
+// TODO: cleanup
+async function validateUsageRemaining(
+  clientSocket: AuthenticatedWebSocket,
+  userId: UserId,
+  onReady: () => void,
+) {
+  const usageData = await usageService.getUsage(userId).catch((err) => {
+    console.error('error when checking usage: ', err);
+    throw err;
+  });
+  if (usageData.remainingMs <= 0) {
+    closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
+      error: 'Exceeded allocated usage',
+      code: InternalErrorCode.ExceededAllocatedUsageError,
+    });
+    return;
+  }
+  onReady();
+  try {
+    await sendData(clientSocket, { event: 'ready' });
+  } catch (err) {
+    console.error('error sending ready: ', err);
+  }
+}
+
+function handleCloseFactory(userId: UserId): () => void {
+  return () => {
+    delete USER_ID_SOCKET_MAP[userId];
+    delete USER_ID_READY_SOCKET_MAP[userId];
+  };
+}
+
+function handleMessageFactory(
+  userId: UserId,
+): (this: AuthenticatedWebSocket, data: RawData) => void {
+  return function (this: AuthenticatedWebSocket, data) {
+    if (!isReady(userId)) {
+      return closeWithError(this, WsCloseCode.PolicyViolation, {
+        error: 'not ready',
+        code: InternalErrorCode.NotReady,
+      });
+    }
+    const buffer = bufferFromRawData(data);
+    const abortController = new AbortController();
+    void (async () => {
+      const unExpectedCloseHandler = () => abortController.abort();
+      this.once('close', unExpectedCloseHandler);
+      try {
+        // TODO: use a queue and offload packets to the filesystem if overloaded, this also ensures transcriptions are processed and returned in order
+        //  or add some form of counter to the requests sent from the client so the server can return them in the correct order
+        // TODO: mutex / lock for each user to prevent over usage or estimate the usage and subtract it from allowance up front
+        const result = await timeout(60_000, (abortSignal) => {
+          return transcribeService.transcribeForUser({
+            audioPacket: buffer,
+            userId,
+            abortSignal,
+          });
+        });
+        if (!isOpen(this)) {
+          // socket closed while we were processing
+          // TODO: cache result for limited time and propagate during reconnect
+          // noinspection ExceptionCaughtLocallyJS
+          throw new ConnectionClosedUnexpectedlyError(
+            'Socket closed while processing',
+          );
+        }
+        await sendData(this, result);
+        if (result.usageRemainingMs <= 0) {
+          return closeWithError(this, WsCloseCode.PolicyViolation, {
+            error: 'Exceeded allocated usage',
+            code: InternalErrorCode.ExceededAllocatedUsageError,
+          });
+        }
+      } catch (err) {
+        if (!isOpen(this)) {
+          // don't attempt to propagate the error if the client disconnected
+          return;
+        }
+        // TODO: refund user if server closing or server error
+        handleTranscribeError(err, this);
+      } finally {
+        this.off('close', unExpectedCloseHandler);
+      }
+    })();
+  };
+}
+
+function isReady(userId: UserId): boolean {
+  return USER_ID_READY_SOCKET_MAP[userId] ?? false;
+}
+
+function handleConnection(
+  clientSocket: AuthenticatedWebSocket,
+  req: IncomingMessage,
+): void {
+  clientSocket.user = authenticateClient(req);
+
+  if (!clientSocket.user) {
+    // Close with policy violation
+    return closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
+      error: 'Unauthorized',
+      code: InternalErrorCode.Unauthorized,
+    });
+  }
+
+  const userId = clientSocket.user.id;
+  registerSocketForUserId(clientSocket, userId);
+  const closeHandler = handleCloseFactory(userId);
+  const messageHandler = handleMessageFactory(userId).bind(clientSocket);
+  clientSocket.once('close', closeHandler);
+  clientSocket.on('message', messageHandler);
+
+  void validateUsageRemaining(clientSocket, userId, () => {
+    USER_ID_READY_SOCKET_MAP[userId] = true;
+  });
+}
+
+function shutdownFactory(wss: WebSocketServer): () => Promise<void> {
+  return async () => {
     wss.clients.forEach((client) => {
       if (client.readyState !== WebSocket.OPEN) {
         return;
@@ -177,103 +296,17 @@ export function setupWebSocket(server: Server) {
         }
       }),
     );
-  }
+  };
+}
 
-  wss.on('connection', (clientSocket: AuthenticatedWebSocket, req) => {
-    let ready = false;
-    clientSocket.user = authenticateClient(req);
-    if (!clientSocket.user) {
-      // Close with policy violation
-      closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
-        error: 'Unauthorized',
-        code: InternalErrorCode.Unauthorized,
-      });
-      return;
-    }
-
-    const user = clientSocket.user;
-
-    registerSocketForUserId(clientSocket, user.id);
-
-    clientSocket.once('close', () => {
-      delete USER_ID_SOCKET_MAP[user.id];
-    });
-
-    clientSocket.on('message', (data) => {
-      if (!ready) {
-        closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
-          error: 'not ready',
-          code: InternalErrorCode.NotReady,
-        });
-        return;
-      }
-      const buffer = bufferFromRawData(data);
-      const abortController = new AbortController();
-      let clientDisconnected = false;
-      void (async () => {
-        try {
-          clientSocket.once('close', () => {
-            clientDisconnected = true;
-            abortController.abort();
-          });
-          // TODO: use a queue and offload packets to the filesystem if overloaded, this also ensures transcriptions are processed and returned in order
-          //  or add some form of counter to the requests sent from the client so the server can return them in the correct order
-          // TODO: mutex / lock for each user to prevent over usage or estimate the usage and subtract it from allowance up front
-          const result = await timeout(60_000, (abortSignal) => {
-            return transcribeService.transcribeForUser({
-              audioPacket: buffer,
-              userId: user.id,
-              abortSignal,
-            });
-          });
-          if (clientSocket.readyState !== WebSocket.OPEN) {
-            // socket closed while we were processing
-            clientDisconnected = true;
-            // TODO: cache result for limited time and propagate during reconnect
-            // noinspection ExceptionCaughtLocallyJS
-            throw new ConnectionClosedUnexpectedlyError(
-              'Socket closed while processing',
-            );
-          }
-          await sendData(clientSocket, result);
-          if (result.usageRemainingMs <= 0) {
-            closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
-              error: 'Exceeded allocated usage',
-              code: InternalErrorCode.ExceededAllocatedUsageError,
-            });
-          }
-        } catch (err) {
-          if (clientDisconnected) {
-            // don't attempt to propagate the error if the client disconnected
-            return;
-          }
-          // TODO: refund user if server closing or server error
-          handleTranscribeError(err, clientSocket);
-        }
-      })();
-    });
-
-    // TODO: cleanup
-    void (async () => {
-      const usageData = await usageService.getUsage(user.id).catch((err) => {
-        console.error('error when checking usage: ', err);
-        throw err;
-      });
-      if (usageData.remainingMs <= 0) {
-        closeWithError(clientSocket, WsCloseCode.PolicyViolation, {
-          error: 'Exceeded allocated usage',
-          code: InternalErrorCode.ExceededAllocatedUsageError,
-        });
-        return;
-      }
-      ready = true;
-      try {
-        await sendData(clientSocket, { event: 'ready' });
-      } catch (err) {
-        console.error('error sending ready: ', err);
-      }
-    })();
+export function setupWebSocket(server: Server) {
+  const wss = new WebSocketServer<AuthenticatedWebSocket>({
+    server,
   });
+
+  wss.on('connection', handleConnection);
+
+  const shutdown = shutdownFactory(wss);
 
   return {
     wss,
